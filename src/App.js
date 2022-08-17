c42555ce93c3f29a4f4c69386b6bedc20cb4d00d
Wen <owilalala974@gmail.com>
2022-08-17 16:32:29 +0800

    new react-0817

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 // import Student from './ex-0817-2/Student';
 // import Product from './ex-0817-3/Product';
 // import Counter from "./ex-0817-4/Counter";
-import Menu from './homework/Menu';
+// import Menu from './homework/Menu';
+import ObjArrayDemo from './ex-0817-5/ObjArrayDemo';
 //JSX語法
 function App() {
   return (
     <>
-      <Menu />
+      <ObjArrayDemo />
     </>
   );
 }
