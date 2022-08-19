ace65a7b79b008c646a09a232452f6aa886f83fa
Wen <owilalala974@gmail.com>
2022-08-19 10:40:50 +0800

    mod ObjArrayDemo 增加後面練習code

--- src/ex-0817-5/ObjArrayDemo.js
+++ src/ex-0817-5/ObjArrayDemo.js
@@ -41,7 +41,7 @@ function ObjArrayDemo() {
         onClick={() => {
           // 先寫出要新增的物件值
           const newObj = { id: 99, text: 'xxx' };
-
+          //immutable
           // 1. 從目前的狀態拷貝出一個新的變數值(陣列/物件)
           // 2. 在新的變數值(陣列/物件)上作處理
           // 3. 設定回原本的狀態中
@@ -81,13 +81,73 @@ function ObjArrayDemo() {
         尋找(過濾)只呈現所有文字有a字母的
       </button>
       <br />
-      <button onClick={() => {}}>刪除文字為b的物件</button>
+      <button
+        onClick={() => {
+          const newData = data.filter((v, i) => {
+            // return !v.text.includes('b');
+            return v.text !== 'b';
+          });
+          setData(newData);
+        }}
+      >
+        刪除文字為b的物件
+      </button>
       <br />
-      <button onClick={() => {}}>刪除id為99的物件</button>
+      <button
+        onClick={() => {
+          const newData = data.filter((v, i) => {
+            return v.id !== 99;
+            // return v.id.includes(99);//這個錯誤
+          });
+          setData(newData);
+        }}
+      >
+        刪除id為99的物件
+      </button>
       <br />
-      <button onClick={() => {}}>在id為2後面插入id為5與文字為bbb的物件</button>
+      <button
+        onClick={() => {
+          //1.建立要插入的物件
+          const newObj = { id: 5, text: 'bbb' };
+          //2.找到id=2的物件位置索引值
+          const index = data.findIndex((v, i) => {
+            return v.id === 2;
+          });
+          //3.如果有找到
+          if (index > -1) {
+            //分割兩個陣列
+            const aa = data.slice(0, index + 1);
+            const ab = data.slice(index + 1);
+            //合併陣列
+            const newData = [...aa, newObj, ...ab];
+            //set新狀態
+            setData(newData);
+          }
+        }}
+      >
+        在id為2後面插入id為5與文字為bbb的物件
+      </button>
       <br />
-      <button onClick={() => {}}>取代id為3的文字為cccc</button>
+      <button
+        onClick={() => {
+          //找id=3的index
+          const index = data.findIndex((v, i) => {
+            return v.id === 3;
+          });
+          if (index !== -1) {
+            const newData = data.map((v) => {
+              return { ...v };
+            });
+            //常見深拷貝
+            // const newData = JSON.parse(JSON.stringify(data));
+            console.log(newData);
+            newData[index].text = 'cccc';
+            setData(newData);
+          }
+        }}
+      >
+        取代id為3的文字為cccc
+      </button>
     </>
   );
 }
