f846b8dc8ab0e2a42ec029b347ce160708608cbd
Wen <owilalala974@gmail.com>
2022-08-29 11:39:52 +0800

    mod 新增 sass 使用方式

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-// import JSXTest from './ex-0817-1/JSXTest';
-// import Student from './ex-0817-2/Student';
-// import Product from './ex-0817-3/Product';
-// import Counter from "./ex-0817-4/Counter";
-// import Menu from './homework/Menu';
-// import ObjArrayDemo from './ex-0817-5/ObjArrayDemo';
-// import Todo from './ex-0819-1/Todo';
-// import Parent from './ex-0819-2/Parent';
-// import Parent from './ex-0822-1/Parent';
-// import TodoApp from './ex-0822-4/TodoApp';
-// import OrderApp from './ex-0822-5/OrderApp';
-// import Controlled from './ex-0822-6/Controlled';
-// import Ex01 from './parctice/Ex01';
-// import Controlled from './ex-0824-1/Controlled';
-// import BirthSelect from './ex-0824-2/BirthSelect';
-// import BMI from './ex-0824-3/BMI';
-// import HTML5Form from './ex-0824-5/HTML5Form';
-// import IdFrom from './ex-0825-1/IdForm';
-// import RefsFrom from './ex-0825-1/RefsFrom';
-// import { useState } from 'react';
-// import FCLifecycle from './ex-0825-2/FCLifecycle';
-import UserList from './ex-0825-4/UserList';
-//JSX語法
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 function App() {
   return (
     <>
-      <UserList />
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h1>React Bootstrap</h1>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="success">Success</Button>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
